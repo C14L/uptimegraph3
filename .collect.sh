21b8f170a1e0587a69e6d3f2d50831880c9e9731
Christian Steimel <me@C14L.com>
2016-01-18 23:07:52 +0100

    collect number of items in mailq

--- .collect.sh
+++ .collect.sh
@@ -3,6 +3,13 @@
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P )
 DATA_DIR="$SCRIPT_DIR/data"
 DATA_FILE="$DATA_DIR/`date --utc +%Y`-`date --utc +%V`.txt"
+MAILQ_FILE="$DATA_DIR/mailq-`date --utc +%Y`-`date --utc +%V`.txt"
 
+# Collect "uptime" stats: 1m 5m 15m avrg load.
+#
 echo "`date +%s` `uptime | grep -ohe 'load average[s:][: ].*' | cut -d' ' -f3,4,5 | sed -e 's/, / /g' | sed -e 's/,/./g'`" >> $DATA_FILE
+
+# Collect "mailq" stats: Number of items in the mailq.
+#
+echo "`date +%s` `mailq | grep -P "^[0-9A-F]{10}\s{2,}" | wc -l`" >> $MAILQ_FILE
 
