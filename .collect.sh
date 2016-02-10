65f198e1960de3667a04d369f1052369bec0b7d8
Christian Steimel <me@C14L.com>
2016-02-10 16:00:49 +0100

    fix for last commit

--- .collect.sh
+++ .collect.sh
@@ -19,15 +19,14 @@ echo "`date +%s` `mailq | grep -P '^[0-9A-F]{10}\s{2,}' | wc -l`" >> $MAILQ_FILE
 ###
 # Collect number of emails sent during the past minute.
 ###
-if [ `date +%M` = '59' ]; then 
-  MON=$(date +%b)
-  DAY=$(date +%e)
-  HOU=$(date +%H)
-  MIN=$(($(date +%M)-1))
-  if [ $MIN == '-1' ]; then HOU=$(($HOU-1)); MIN='59'; fi
-  if [ $HOU == '-1' ]; then DAY=$(($DAY-1)); HOU='23'; fi
-  #
-  # This leaves a gap for the last minute of every month. That's okay.
-  #
-  echo "`date +%s` `cat /var/log/mail.log | grep -P "$MON\s+$DAY\s+$HOU:$MIN:" | grep 'status=sent' | wc -l`" >> $SENT_FILE
-fi
+MON=$(date +%b)
+DAY=$(date +%e)
+HOU=$(date +%H)
+MIN=$(($(date +%M)-1))
+if [ $MIN == '-1' ]; then HOU=$(($HOU-1)); MIN='59'; fi
+if [ $HOU == '-1' ]; then DAY=$(($DAY-1)); HOU='23'; fi
+#
+# This leaves a gap for the last minute of every month. That's okay.
+#
+echo "`date +%s` `cat /var/log/mail.log | grep -P "$MON\s+$DAY\s+$HOU:$MIN:" | grep 'status=sent' | wc -l`" >> $SENT_FILE
+
